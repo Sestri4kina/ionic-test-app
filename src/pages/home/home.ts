cfcf473ff50b2e3227247bfd45515a4802177a94
Anastasiia Chepka <abratochkina@gmail.com>
2018-03-19 22:48:12 +0200

    Add functionality to add two numbers

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { ViewController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+ calculatorForm: FormGroup;
+ result: number;
+ @ViewChild('result') resultOutput: ElementRef;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public viewController: ViewController,
+    private formBuilder: FormBuilder,
+  ) {
+    this.calculatorForm = this.formBuilder.group({
+      firstValue: ['', Validators.required],
+      secondValue: ['', Validators.required],
+    });
+  }
 
+  addValues() {
+    console.log(this.calculatorForm.value);
+    const data = this.calculatorForm.value,
+      firstNum = +data.firstValue, secondNum = +data.secondValue;
+
+    this.resultOutput.nativeElement.textContent = firstNum + secondNum;
   }
 
 }
